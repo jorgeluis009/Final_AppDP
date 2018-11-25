ef1e3f4c5030ccd1101890b255741e9d774ca500
ManuGlez1 <0192883@up.edu.mx>
2018-11-24 23:18:44 -0600

    Lectura y escritura de codigos QR version 1

--- Final_AppDP/Welcome.Designer.cs
+++ Final_AppDP/Welcome.Designer.cs
@@ -29,8 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,38 +40,59 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(69, 9);
+            this.label1.Location = new System.Drawing.Point(162, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(182, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Read QR Codes";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Final_AppDP.Properties.Resources.QR;
-            this.pictureBox1.Location = new System.Drawing.Point(95, 60);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(125, 109);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(59, 194);
+            this.label2.Location = new System.Drawing.Point(201, 63);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(192, 21);
+            this.label2.Size = new System.Drawing.Size(80, 21);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Click the Image Above";
+            this.label2.Text = "Click me!";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 21);
+            this.label3.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Final_AppDP.Properties.Resources.QR;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Create a QR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Welcome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(319, 239);
+            this.ClientSize = new System.Drawing.Size(462, 222);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
@@ -84,8 +107,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Final_AppDP/Welcome.cs
+++ Final_AppDP/Welcome.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QRCodeEncoderDecoderLibrary;
 
 namespace Final_AppDP
 {
@@ -15,21 +18,43 @@ namespace Final_AppDP
         public Welcome()
         {
             InitializeComponent();
-        }
+        }        
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
-            // open file dialog   
             OpenFileDialog open = new OpenFileDialog();
-            // image filters  
-            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            open.Multiselect = true;
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            QRDecoder QRCodeDecoder = new QRDecoder();
+            BindingList<string> orders = new BindingList<string>();
+            string order = "";
             if (open.ShowDialog() == DialogResult.OK)
             {
-                // display image in picture box  
-                pictureBox1.Image = new Bitmap(open.FileName);
-                // image file path  
-                label1.Text = open.FileName;
+                foreach (String file in open.FileNames)
+                {
+                    Bitmap QRImg = new Bitmap(file);
+                    //pictureBox1.Image = new Bitmap(QRImg);                    
+                    byte[][] DataByteArray = QRCodeDecoder.ImageDecoder(QRImg);
+                    try
+                    {                        
+                        string Result = QRCode.ByteArrayToStr(DataByteArray[0]);
+                        order += Result + "\n";
+                        orders.Add(Result);                        
+                    }
+                    catch (Exception){ }
+                }
             }
-        }        
+            label3.Text = order;                      
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QREncoder QRCodeEncoder = new QREncoder();            
+            QRCodeEncoder.Encode(ErrorCorrection.M, "This is a message, hello.");            
+            Bitmap QRCodeImage = QRCodeToBitmap.CreateBitmap(QRCodeEncoder, 4, 8);            
+            FileStream FS = new FileStream("TestQR.png", FileMode.Create);
+            QRCodeImage.Save(FS, ImageFormat.Png);
+            FS.Close();
+        }
     }
 }
